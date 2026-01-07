2d33933fff9a8d82acca2949b634dae9b916cbb8
Gustavo Aguilar <gustavo.aguilar@gmail.com>
2026-01-07 22:26:27 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from './utils';
 import { base44 } from '@/api/base44Client';
-import { useAuthContext, AuthProvider } from './components/contexts/AuthContext';
+import { useAuthContext } from './components/contexts/AuthContext';
 import ImpersonationBanner from './components/superadmin/ImpersonationBanner';
 import {
   LayoutDashboard,
@@ -23,7 +23,7 @@ import {
 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
-function LayoutContent({ children, currentPageName }) {
+export default function Layout({ children, currentPageName }) {
   const [sidebarOpen, setSidebarOpen] = useState(true);
   const { user, effectiveRole, isImpersonating, effectiveOrgId, refreshAuth } = useAuthContext();
 
@@ -232,12 +232,4 @@ return (
     </div>
   </>
 );
-}
-
-export default function Layout(props) {
-  return (
-    <AuthProvider>
-      <LayoutContent {...props} />
-    </AuthProvider>
-  );
 }

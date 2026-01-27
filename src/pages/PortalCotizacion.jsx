6ec4b02df86fe67046d16fe2a420094612f7e339
Gustavo Aguilar <gustavo.aguilar@gmail.com>
2026-01-27 21:26:06 +0000

    File changes

--- src/pages/PortalCotizacion.jsx
+++ src/pages/PortalCotizacion.jsx
@@ -18,6 +18,7 @@ import { format } from 'date-fns';
 import { es } from 'date-fns/locale';
 import { Button } from '@/components/ui/button';
 import jsPDF from 'jspdf';
+import { getPublicBaseUrl } from '@/components/ventas/getPublicBaseUrl';
 
 const estadoConfig = {
   borrador: { 

4940364a2098278656a4de3b7c057eb1ac7f608e
Gustavo Aguilar <gustavo.aguilar@gmail.com>
2026-04-30 20:26:19 +0000

    Manual code change

--- src/pages/OrdenesTrabajo.jsx
+++ src/pages/OrdenesTrabajo.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useAuthContext } from "@/contexts/AuthContext";
+import { useAuthContext } from "@/components/hooks/useAuthContext";
 import { sotFetch } from "@/lib/sotFetch";
 
 import FormularioCliente from "@/components/clientes/FormularioCliente";
